558a4645fc9e3e5bf3eaf792cdcab13ae7e0440c
Ruslan Rybak <ruslan.rybak@gmail.com>
2023-02-10 23:20:41 +0000

    testing game lifecycle

--- classes/World.js
+++ classes/World.js
@@ -14,19 +14,20 @@ import WorldObject from "./WorldObject";
 */
 class World {
     constructor(dx = 10, dy = 10) {
-        this.dx = dx;
+        this.RUNNING = false
+        this.dx = dx
         this.dy = dy
-        this.objects = [];
-        this.map = [];
-        this.gameTick = 0;
-        this.constructMap(dx, dy);
+        this.objects = []
+        this.map = []
+        this.gameTick = 0
+        this.constructMap(dx, dy)
     }
     addObject(point, type) {
-       const obj = new WorldObject(point, type);
-       this.objects.push(obj);
+       const obj = new WorldObject(point, type)
+       this.objects.push(obj)
     }
     printToConsole(dx, dy) {
-        let consoleOutput = "";
+        let consoleOutput = ""
         for (let y = 0; y < dy; y++) {
             for (let x = 0; x < dx; x++) {
                 consoleOutput += this.map[x][y]
@@ -36,29 +37,62 @@ class World {
         console.table(consoleOutput);
         // console.log(this.map);      
     }
-    
     printToMap(x, y, char) {
-        this.map[x][y] = char;
+        this.map[x][y] = char
     }
-    
+    createRandomObject() {
+        this.addObject({x: this.random(300), y: this.random(300)})
+    }
+    /**
+     * Create random positioned objects
+     * @param {number} num number of objects to create
+     * @returns void
+     */
+    createRandomObjects(num) {
+        for (let i = 0; i < num; i++ ) {
+            this.createRandomObject()
+        }
+    }
     constructMap(dx, dy) {
-        this.map = [];
+        this.map = []
         
         for (let y = 0; y < dy; y++) {
-            let row = [];
+            let row = []
             for (let x = 0; x < dx; x++) {
-                let sign = ".";
+                let sign = "."
                 // empty
                 if (y == 0 && x == 0) {
-                    sign = "0";
+                    sign = "0"
                 }
                 row.push(sign)
-                // console.log("row", row);
+                // console.log("row", row)
             }
-        this.map.push(row);
+        this.map.push(row)
         }
         // console.log("map", this.map)
     }
+    moveObjects() {
+        this.game.objects.forEach(gObj => {
+            // console.log(6 - this.random(12))
+            gObj.coord.x = gObj.coord.x + 1 - this.random(3)
+            gObj.coord.y = gObj.coord.y + 1 - this.random(3)
+        });
+        if (this.RUNNING) {
+            setTimeout(() => this.moveObjects(), 100)
+        }
+        
+    }
+    random() {
+        return Math.floor(Math.random() * x)
+    }
+    start() {
+        this.RUNNING = true
+        this.moveObjects()
+    }
+    stop() {
+        this.RUNNING = false
+    }
+
 }
 
 export {
